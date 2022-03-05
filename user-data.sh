ea222595bd17f7f62a141b5ff06591fe3f034d62
leonelvargas <63625333+leonelvargas@users.noreply.github.com>
2022-03-05 13:33:15 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -12,13 +12,7 @@ sudo apt install -y net-tools acl unzip
 #Create the Pyplan folder and give access to the current user.
 sudo mkdir -p /pyplan/deploy && sudo setfacl -R -m u:pyplan:rwx,d:u:pyplan:rwx /pyplan
 #Azure CLI
-sudo apt install ca-certificates curl apt-transport-https lsb-release gnupg -y
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-a="deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main"
-echo $a | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt update -y
-sudo apt install azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #Kubectl CLI
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 b="deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main"
